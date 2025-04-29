a68481dd21557e4ca2c617d567ad4c708d83656d
github-ci <you@example.com>
2025-04-29 13:12:14 +0000

    fix(packages/workerman-connection-pipe): 修改 MessageWatcherInterface 返回类型

--- src/Watcher/MessageWatcherInterface.php
+++ src/Watcher/MessageWatcherInterface.php
@@ -6,5 +6,5 @@ use Workerman\Connection\ConnectionInterface;
 
 interface MessageWatcherInterface
 {
-    public function __invoke(mixed $data, ConnectionInterface $source, ConnectionInterface $target, callable $resolver): bool;
+    public function __invoke(mixed $data, ConnectionInterface $source, ConnectionInterface $target, callable $resolver): void;
 }
